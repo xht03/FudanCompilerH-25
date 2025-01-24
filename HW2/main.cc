bfa426ca97fb482357a06119bdf9301634616882
王晓阳 <xywangcs@fudan.edu.cn>
2025-01-24 21:39:55 +0800

    Add HW2

---
+++ HW2/main.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int parse_file(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Could not open the file!" << std::endl;
+        return -1;
+    }
+
+    std::string word;
+    int wordCount = 0;
+    while (file >> word) {
+        std::cout << word << " ";
+        ++wordCount;
+    }
+    std::cout << std::endl;
+
+    file.close();
+    return wordCount;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    int result = parse_file(filename);
+
+    std::cout << "The result is: " << result << std::endl;
+
+    return 0;
+}
