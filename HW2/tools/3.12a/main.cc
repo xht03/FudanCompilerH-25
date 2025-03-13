8043a4d41c438546d8d83f0f5098ead455d5caab
xht03 <1620318777@qq.com>
2025-03-13 16:55:36 +0800

    lab2 done!

--- HW2/tools/3.12a/main.cc
+++ HW2/tools/3.12a/main.cc
@@ -1,73 +1,142 @@
-#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <vector>
+#include <stack>
+#include <map>
 
 using namespace std;
 
-enum TokenType { S, E, ID, PLUS, LPAREN, RPAREN, END };
+string action;                      // 解析动作 (shift, reduce, accept)
+pair<string, string> prod;          // 当前产生式
+int state, curState, newState;      // 解析状态，栈顶状态，规约后新状态
 
-int parse_file(const std::string& filename)
+// 产生式
+const map<int, pair<string, string>> prods = {
+    { 1, { "S", "E" } },
+    { 2, { "E", "i" } },
+    { 3, { "E", "i(E)" } },
+    { 4, { "E", "E+i" } },
+};
+
+// 分析表
+const map<pair<int, char>, string> table = {
+
+    // 状态0
+    // S -> ·E
+    // E -> ·i
+    // E -> ·i(E)
+    // E -> ·E+i
+    { { 0, 'i' }, "s3" },
+    { { 0, 'E' }, "1" },
+    { { 0, 'S' }, "2" },
+
+    // 状态1
+    // S -> E·
+    // E -> E·+i
+    { { 1, '+' }, "s4" },
+
+    { { 1, '$' }, "r1" },
+    { { 1, 'i' }, "r1" },
+    { { 1, '(' }, "r1" },
+    { { 1, ')' }, "r1" },
+
+    // 状态2
+    // S' -> S·
+    { { 2, '$' }, "$" },
+
+    // 状态3
+    // E -> i·
+    // E -> i·(E)
+    { { 3, '(' }, "s5" },
+
+    { { 3, ')' }, "r2" },
+    { { 3, '+' }, "r2" },
+    { { 3, 'i' }, "r2" },
+    { { 3, '$' }, "r2" },
+
+    // 状态4
+    // E -> E+·i
+    { { 4, 'i' }, "s6" },
+
+    // 状态5
+    // E -> i(·E)
+    // E -> ·i
+    // E -> ·i(E)
+    // E -> ·E+i
+    { { 5, 'i' }, "s3" },
+    { { 5, 'E' }, "7" },
+
+    // 状态6
+    // E -> E+i·
+    { { 6, '(' }, "r4" },
+    { { 6, ')' }, "r4" },
+    { { 6, '+' }, "r4" },
+    { { 6, 'i' }, "r4" },
+    { { 6, '$' }, "r4" },
+
+    // 状态7
+    // E -> i(E·)
+    // E -> E·+i
+    { { 7, ')' }, "s8" },
+    { { 7, '+' }, "s4" },
+
+    // 状态8
+    // E -> i(E)·
+    { { 8, '(' }, "r3" },
+    { { 8, ')' }, "r3" },
+    { { 8, 'i' }, "r3" },
+    { { 8, '+' }, "r3" },
+    { { 8, '$' }, "r3" },
+};
+
+int main()
 {
-    ifstream file(filename);
-    if (!file.is_open()) {
-        cerr << "Could not open the file!" << endl;
-        return -1;
-    }
-
-    streamsize size = file.tellg();
-    file.seekg(0, ios::beg);
-
-    char* buffer = new char[size + 1];
-    if (!file.read(buffer, size)) {
-        cerr << "Could not read the file!" << endl;
-        return -1;
-    }
-    buffer[size] = '\0';
-
-    vector<TokenType> tokens;
-    for (int i = 0; i < size; i++) {
-        switch (buffer[i]) {
-            case 'i':
-                tokens.push_back(ID);
-                break;
-            case 'd':
-                break;
-            case '+':
-                tokens.push_back(PLUS);
-                break;
-            case '(':
-                tokens.push_back(LPAREN);
-                break;
-            case ')':
-                tokens.push_back(RPAREN);
-                break;
-            case '$':
-                tokens.push_back(END);
-                break;
-            default:
-                cerr << "Invalid character in the file!" << endl;
-                return -1;
+    string str;
+    cin >> str;
+    // 将字符串中的id替换成i
+    for (int i = 0; i < str.length(); i++)
+        if (str[i] == 'i' && str[i + 1] == 'd') {
+            str[i] = 'i';
+            str.erase(i + 1, 1);
         }
+    str += "$";
+
+    // <字符, 状态号> 解析栈
+    stack<pair<char, int>> Stack;
+    Stack.push({ '*', 0 });
+
+    try {
+        for (char c : str) {
+        PARSE:
+            state = Stack.top().second;         // 获取当前状态
+            action = table.at({ state, c });    // 获取对应动作
+
+            switch (action[0]) {
+                case 's': // 移入字符与新状态
+                    Stack.push({ c, stoi(action.substr(1)) });
+                    continue;
+
+                case 'r': // 进行规约
+                    prod = prods.at(stoi(action.substr(1)));
+
+                    // 弹出栈顶处的产生式右侧元素
+                    for (int i = 0; i < prod.second.length(); i++)
+                        Stack.pop();
+
+                    // 压入规约后的新元素, 并更新状态
+                    curState = Stack.top().second;
+                    newState = stoi(table.at({ curState, prod.first[0] }));
+                    Stack.push({ prod.first[0], newState });
+
+                    // 重新解析
+                    goto PARSE;
+
+                case '$':
+                    cout << "accept\n";
+                    exit(0);
+            }
+        }
+    } catch (...) {
+        cout << "reject\n";
+        exit(0);
     }
-
-    file.close();
-    delete[] buffer;
-    
-}
-
-int main(int argc, char* argv[])
-{
-    if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " <filename>" << endl;
-        return 1;
-    }
-
-    string filename = argv[1];
-    int result = parse_file(filename);
-
-    cout << "The result is: " << result << endl;
-
-    return 0;
 }
